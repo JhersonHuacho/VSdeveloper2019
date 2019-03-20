a2da9641eb01802289119391b678d0ebb2e06d88
YasminHerrera <roxanayashl@gmail.com>
2019-03-19 22:28:27 -0500

    capítulo 08 - IIS - WCF
    publicar servicios wcf en el IIS

---
+++ Cap08/slnApp/App.Service.WCFAppTest/ArtistServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App.Service.WCFAppTest
+{
+    [TestClass]
+    public class ArtistServiceTest
+    {
+        [TestMethod]
+        public void GetArtistAllTest()
+        {
+            var client = new MantenimientoServices.MantenimientoServicesClient();
+            var lista = client.GetArtistAll("a");
+
+            Assert.IsTrue(lista.Count > 0);
+        }
+    }
+}
