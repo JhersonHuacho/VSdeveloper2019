adc5255cbcee9a56985c6f2ab117bb5e3ad7c5a3
YasminHerrera <roxanayashl@gmail.com>
2019-03-26 22:23:16 -0500

    Clase 09
    html, css, bootstrap

---
+++ Cap09/slnApp/App.DataAccess.Repository.Test/AppUnitOfWorkTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using App.DataAccess.Repository.Interface;
+
+namespace App.DataAccess.Repository.Test
+{
+    /// <summary>
+    /// Descripción resumida de AppUnitOfWorkTest
+    /// </summary>
+    [TestClass]
+    public class AppUnitOfWorkTest
+    {
+        [TestMethod]
+        public void ExistenArtistas()
+        {
+            using (var unitOfWork = new AppUnitOfWork())
+            {
+                var cantRows = unitOfWork.ArtistRepository.Count();
+                Assert.IsTrue(cantRows > 0);
+            }
+        }
+    }
+}
---
+++ Cap09/slnApp/App.Domain/ArtistDomain.cs
@@ -0,0 +1,27 @@
+﻿using App.DataAccess.Repository;
+using App.Domain.Interfaces;
+using App.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain
+{
+    public class ArtistDomain : IArtistDomain
+    {
+        public IEnumerable<Artist> GetArtists(string nombre)
+        {
+            IEnumerable<Artist> result = new List<Artist>();
+
+            using (AppUnitOfWork uw = new AppUnitOfWork())
+            {
+                //result = uw.ArtistRepository.GetAll();
+                result = uw.ArtistRepository.GetAll(item => item.Name.Contains(nombre));
+            }
+
+            return result;
+        }
+    }
+}
